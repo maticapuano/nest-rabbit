d66595d4fbf1d189b2fa659514b9456506fc46d7
Matias Capuano <maticapuano97@gmail.com>
2025-05-02 15:56:18 -0300

    Refactor exports in index.ts to include additional queue-related decorators and options for improved functionality.

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,4 +1,19 @@
 export { QUEUE_METADATA } from "./constants/metadata.constants";
-export { QueueOptions } from "./decorators";
+export {
+  OnMessageFailed,
+  OnMessageProcessed,
+  OnMessageReceived,
+  OnQueueError,
+  OnQueueEvent,
+  Process,
+  PROCESS_METADATA,
+  ProcessOptions,
+  Queue,
+  QUEUE_EVENTS_METADATA,
+  QueueEventOptions,
+  QueueEventType,
+  QueueOptions,
+  RetryOptions,
+} from "./decorators";
 export * from "./interfaces";
 export * from "./services";
